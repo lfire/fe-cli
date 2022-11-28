2227cc2416499e164741a977b9e2e9d8b9d5aa21
benjilli <benjilli@tencent.com>
2022-11-28 19:16:51 +0800

    feat: init project

---
+++ bin/index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const pkg = require('../package.json');
+
+// // 直接使用 commander 实例
+// const { program } = commander;
+
+// 手工自建一个实例
+const program = new commander.Command();
+
+program.showHelpAfterError();
+program
+  .name(Object.keys(pkg.bin)[0])
+  .usage('<command> [options]')
+  .version(pkg.version)
+  .option('-d, --debug', '是否开启调试', false)
+  .option('-e, --env <env>', '获取环境变量');
+
+// 获取配置项
+const options = program.opts();
+
+// 注册命令
+// 1. command 注册命令
+const clone = program.command('clone <source> [destination]');
+clone
+  .description('clone a repository from remote')
+  .option('-f, --force', '是否强制clone')
+  .action((source, destination, cmdOpt) => {
+    console.log(`do clone from ${source} to ${destination}`, cmdOpt);
+  });
+
+// 2. addCommand 注册子命令
+const service = new commander.Command('service');
+service.showHelpAfterError();
+service.showSuggestionAfterError();
+service
+  .command('start [port]')
+  .description('start service on some port, default 8888')
+  .action((port = 8888) => {
+    console.log(`start service on port ${port}`);
+  });
+service
+  .command('stop')
+  .description('stop service')
+  .action(() => {
+    console.log('stop the service');
+  });
+program.addCommand(service);
+
+// 命令行参数自动匹配
+program
+  .arguments('<command> [options]')
+  .description('front-end cli')
+  .action((command, cmdOpt) => {
+    console.log(`run ${command} with ${cmdOpt}`);
+  });
+
+// 解析参数
+// console.log(process.argv);
+program.parse(process.argv);
+
+// program.outputHelp();
+
